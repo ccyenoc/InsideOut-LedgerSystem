acf8fc890bda5250bc821d20de79efcba7fd7cf6
CHIN YIU ERN <yiuernnn@gmail.com>
2025-01-05 00:59:08 +0800

    update

--- src/insideout/Reminder.java
+++ src/insideout/Reminder.java
@@ -29,9 +29,12 @@ public class Reminder {
     String[] repaymentPeriod;
     String[] remainingPayment;
     final String filename = "src/creditloan-apply.csv";
-  //  private static final String EMAIL_FROM = "woiaijdwj@gmail.com";
-    private static final String APP_PASSWORD = "cwwl zggf qwwf bcdm";
+    final String userFileName = "src/userinfo - Sheet1.csv";
+    public String EMAIL_TO;
     private static Label lbl;
+    static final String EMAIL_FROM = "lojingyang051104@gmail.com";
+    static final String APP_PASSWORD = "hcaz vsjh ngsx eixs";
+
 
     public Reminder(){
         try {
@@ -72,12 +75,12 @@ public class Reminder {
     }
     public void notification(String username) throws Exception{
         Date duedate=new Date();
-        String remainingPayment  = "";
+        String remainingPayment  = "0";
 
         for (int i = 0 ; i < userID.length ; i ++ ){
             //Actually its not useerID ...
             if (username.equals(userID[i])){
-                 SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+                 SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd yyyy");
                  duedate = dateFormat.parse(overdue[i]);
                  remainingPayment = this.remainingPayment[i];
                 break;
@@ -85,14 +88,26 @@ public class Reminder {
         }
         Date currentTime =new Date();
 
-        int compare=currentTime.compareTo(duedate);
-        if (currentTime.after(duedate) || currentTime.equals(duedate)){
+        int compare = currentTime.compareTo(duedate);
+        if (currentTime.before(duedate) || currentTime.equals(duedate)){
             Message message =new MimeMessage(getEmailSession());
-         //   message.setFrom(new InternetAddress(EMAIL_FROM));
-           // String EMAIL_TO = "liangyao0808@gmail.com";
-           // message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL_TO));
+            message.setFrom(new InternetAddress(EMAIL_FROM));
+            getUserEmail(username);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(this.EMAIL_TO));
             message.setSubject("Remaining Payment");
             message.setText("RM" + remainingPayment + " required to pay before " + duedate);
+            Transport.send(message);
+            lbl=new Label("Reminder!\nAuto-deduction from balance will be taken to pay for loan(s)");
+        }
+        else if (currentTime.after(duedate)){
+            Message message =new MimeMessage(getEmailSession());
+            message.setFrom(new InternetAddress(EMAIL_FROM));
+            getUserEmail(username);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(this.EMAIL_TO));
+            message.setSubject("OverDue Payment");
+            message.setText("Overdue payment: " + "\n");
+            message.setText("You are not allowed to use the function of credit and debit.");
+            message.setText("RM" + remainingPayment + " required to pay ASAP.");
             Transport.send(message);
             lbl=new Label("Reminder!\nAuto-deduction from balance will be taken to pay for loan(s)");
         }
@@ -101,9 +116,9 @@ public class Reminder {
    
     private static Session getEmailSession(){
         return Session.getInstance(getGmailProperties(), new Authenticator() {
-          //  protected PasswordAuthentication getPasswordAuthentication(){
-             //   return new PasswordAuthentication(EMAIL_FROM , APP_PASSWORD);
-           // }
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(EMAIL_FROM , APP_PASSWORD);
+            }
         });
     }
 
@@ -115,6 +130,26 @@ public class Reminder {
         prop.put("mail.smtp.port" , "587");
         prop.put("mail.smtp.ssl.trust" , "smtp.gmail.com");
         return prop;
+    }
+
+    public void getUserEmail(String username){
+        try{
+            Scanner reader = new Scanner (new FileReader (userFileName));
+            reader.nextLine();
+
+            while(reader.hasNextLine()){
+                String[] line = reader.nextLine().split(",");
+
+                if (line[0].equals(username)){
+                    this.EMAIL_TO = line[2];
+                }
+            }
+
+            reader.close();
+        }
+        catch (Exception e){
+            System.out.print(e.getMessage());
+        }
     }
 
     public static Label getLabel(){
